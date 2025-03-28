401ca9b2b3d6afc7db7118e6d577a1477f7f72a6
MansiSelmohakar <mansiselmohkar1999@gmail.com>
2025-03-28 08:32:04 +0530

    Apple16

---
+++ Apple16.java
@@ -0,0 +1,10 @@
+
+public class Apple16 {
+
+	
+	void SellingPriceCompute(int mrp, int disc) {
+	
+	int sellingprice= mrp - (mrp)*(disc+100);
+	System.out.println(sellingprice);
+}
+}
---
+++ TestApple16.java
@@ -0,0 +1,10 @@
+
+public class TestApple16 {
+public static void main(String[] args) {
+	
+	
+	Apple16 a1= new Apple16();
+	
+	a1.SellingPriceCompute(200, 10);
+}
+}
