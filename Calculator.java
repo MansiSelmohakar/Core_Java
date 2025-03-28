297151f7b2fef6216457d86a00c874a1f6cd07b6
MansiSelmohakar <mansiselmohkar1999@gmail.com>
2025-03-28 08:21:54 +0530

    First Calculator Program

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+
+public class Calculator {
+	
+	
+	int price=999;
+	String name="CASIO";
+	
+	void addTwo(int num1, int num2) {
+		
+		
+		int res=num1+num2;
+		System.out.println(res);
+	}
+	
+	
+
+}
